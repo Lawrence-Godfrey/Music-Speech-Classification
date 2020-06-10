443ce3be8a7f6e27233053cd24a9cf6077e1487a
Lawrence <lawrencegodfrey58@gmail.com>
2020-06-10 11:36:24 +0200

    more testing on larger datasets

--- convert_to_wav.sh
+++ convert_to_wav.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in *.mp3; do mpg321 -w "`basename "$i" .mp3`".wav "$i"; done
+for i in Music_1/**.mp3; do mpg321 -w ~/Uni/DSP/DSP_Project/Music_1/"`basename "$i" .mp3`".wav "$i"; done
